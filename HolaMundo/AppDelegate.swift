52e2b5f378e3a426dc0c60601525ed06d3e7e109
Juan Rodríguez Baeza <juan@macbook-pro-de-juan.local>
2018-05-09 16:36:29 +0200

    Initial Commit

---
+++ HolaMundo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HolaMundo
+//
+//  Created by Juan Rodríguez Baeza on 9/5/18.
+//  Copyright © 2018 Juan Rodríguez Baeza. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HolaMundo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HolaMundo
+//
+//  Created by Juan Rodríguez Baeza on 9/5/18.
+//  Copyright © 2018 Juan Rodríguez Baeza. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
